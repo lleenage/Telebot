29be07558e4bdc724c1321f00852609df32d2c7d
Poleen <fxcklleewyb@gmail.com>
2025-02-02 15:22:49 +0300

    changed main

--- PyCode/bot_func.py
+++ PyCode/bot_func.py
@@ -1,34 +1,19 @@
 import telebot
-import random
-from dotenv import load_dotenv
-import os
-from telebot import types, custom_filters
+from telebot import types
 from telebot.handler_backends import StatesGroup, State
-from sqlalchemy.orm import sessionmaker
-from telebot.storage import StateMemoryStorage
+import random
 
-from main import engine
-from sql_func import User, Personal_words, Basic_words
+from private.confirm import TOKEN_TELEBOT
+from PyCode.main import english_cards, russian_cards
+from PyCode.main import add_users, add_personal_word, get_target_word, \
+    personal_russian_words, personal_english_words, delete_word
 
-dotenv_path = "../private/confirm.env"
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
 
-telebot_token = os.getenv('TOKEN_TELEBOT')
-
-state_storage = StateMemoryStorage()
+telebot_token = TOKEN_TELEBOT
 bot = telebot.TeleBot(telebot_token)
 
-
-known_users = []
-userStep = {}
 buttons = []
-personal_russian_words = []
-personal_english_words = []
-
-basic_russian_words = [i.russian_word for i in (sessionmaker(bind=engine))().query(Basic_words).all()]
-basic_english_words = [i.english_word for i in (sessionmaker(bind=engine))().query(Basic_words).all()]  #передаем сюда и базовые и персональные русские слова
-(sessionmaker(bind=engine))().close()
+known_users = []
 
 class Commands:
     add = 'Добавить слово ➕🆕'
@@ -40,92 +25,6 @@ class MyStates(StatesGroup):
     translate_word = State()
     other_words = State()
 
-def get_target_word(translate_word: str) -> str:
-    session = (sessionmaker(bind=engine))()
-    if translate_word in basic_russian_words:
-        target_w = session.query(Basic_words.english_word).filter(Basic_words.russian_word == translate_word).all()[0][0]
-        session.close()
-        return target_w
-    elif translate_word in basic_english_words:
-        target_w = session.query(Basic_words.russian_word).filter(Basic_words.english_word == translate_word).all()[0][0]
-        session.close()
-        return target_w
-    elif translate_word in personal_english_words:
-        target_w = session.query(Personal_words.russian_word).filter(Personal_words.english_word == translate_word).all()[0][0]
-        session.close()
-        return target_w
-    elif translate_word in personal_russian_words:
-        target_w = session.query(Personal_words.english_word).filter(Personal_words.russian_word == translate_word).all()[0][0]
-        session.close()
-        return target_w
-    else:
-        return 'Нет такого слова'
-
-def get_user_step(user_name: str, user_id: int) -> int:
-    if user_id in userStep:
-        return userStep[user_id] #должен быть 0
-    else:
-        known_users.extend([user_name, user_id])
-        userStep[user_id] = 0
-        print('Новый пользователь, который eще не использовал "/start"')
-        return 0
-
-def add_users(engine, user_id: int, user_name: str):   #функция добавляющая пользователя в бд с параметрами user_id, user_name
-    session = (sessionmaker(bind=engine))()
-    session.add(User(user_id=user_id, name=user_name))
-    for i in session.query(User).all():
-        print(i)
-    session.commit()
-    session.close()
-
-def add_personal_word(engine, any_russian_word: str, any_english_word: str, uid: int) -> str: #функция для добавления слов в бд
-    session = (sessionmaker(bind=engine))()
-    for u_id in session.query(User.id).filter(User.user_id == uid).all():
-        if any_russian_word not in session.query(Personal_words.russian_word).all():
-            session.add(Personal_words(russian_word=any_russian_word, english_word=any_english_word, user_id=u_id[0]))
-            session.commit()
-            for i in session.query(Personal_words).filter(Personal_words.russian_word == any_russian_word).all():
-                result = f'{i.russian_word} - {i.english_word}'
-                session.close()
-                personal_russian_words.append(any_russian_word)
-                personal_english_words.append(any_english_word)
-                return f'"{result}" успешно добавлено.'
-        else:
-            return f'Слово "{any_russian_word}" уже было добавлено ранее'
-
-
-def delete_word(del_word: str, uid: int) -> int:
-    session = (sessionmaker(bind=engine))()
-    for id_ in session.query(User.id).filter(User.user_id == uid).all():
-        id_ = id_[0]
-        if del_word in basic_english_words or del_word in basic_russian_words:
-            print(f'Попытка удалить слово "{del_word}", что находится в базовых словах.')
-            return 0
-        elif del_word in personal_russian_words:
-            session.query(Personal_words).filter(Personal_words.russian_word == del_word).filter(Personal_words.user_id==id_).delete() # удаляем объект
-            session.commit()
-            session.close()
-            del_eng_word = get_target_word(del_word)
-            personal_russian_words.remove(del_word)
-            personal_english_words.remove(del_eng_word)
-            print(f'Удалили слово "{del_word}" из бд')
-            return session.query(Personal_words).filter(Personal_words.user_id==id_).all()
-        elif del_word in personal_english_words:
-            session.query(Personal_words).filter(Personal_words.english_word == del_word).filter(Personal_words.user_id==id_).delete() # удаляем объект
-            session.commit()
-            session.close()
-            print('Этап 1', 'Удалили из бд')
-            del_russ_word = get_target_word(del_word)
-            print('Этап 2', del_russ_word)
-            personal_english_words.remove(del_word)
-            print('Этап 3', personal_english_words)
-            personal_russian_words.remove(del_russ_word)
-            print(f'Удалили слово "{del_word}" из бд')
-            return session.query(Personal_words).filter(Personal_words.user_id==id_).all()
-        else:
-            print(f'Попытка удалить слово "{del_word}", которое не существует ни в одном из бд.')
-            return 1
-
 
 @bot.message_handler(commands=['start'])   #начало работы с ботом, комнда "start"
 def send_welcome(message):
@@ -133,8 +32,8 @@ def send_welcome(message):
     user_name = message.chat.username
     if user_id not in known_users:
         known_users.extend([user_name, user_id])
-        add_users(engine, user_id, user_name) #добавляем пользователя в бд
-        userStep[user_id] = 0
+        add_users(user_id, user_name) #добавляем пользователя в бд
+        # userStep[user_id] = 0
         bot.send_message(user_id, f'''Привет, {user_name}! Добро пожаловать в Бот!
                                     \nОтправь /help и выбери одну, чтобы ознакомиться с правилами!''')
 
@@ -153,8 +52,8 @@ def send_help(message):
 @bot.message_handler(commands=['cards'])    #соманда "cards"
 def buttons_card(message):
     chat_id = message.chat.id
-    #Проверяем наличие необходимого минимума слов(4)
-    words = random.sample(basic_english_words, 4)
+    #Проверяем наличие необходимого минимума английских слов(4)
+    words = random.sample(english_cards, 4)
 
     if not words or len(words) < 4:
         bot.send_message(chat_id, 'Недостаточно слов для создания карточек!\nДобавьте новые через "Добавить слово ➕🆕".')
@@ -163,9 +62,9 @@ def buttons_card(message):
         global buttons
         buttons = []
         markup = types.ReplyKeyboardMarkup(row_width=2)
-        translate_word = random.choice(basic_russian_words)
+        translate_word = random.choice(russian_cards)
         target_word = get_target_word(translate_word)
-        other_words = random.sample(basic_english_words, 3)
+        other_words = random.sample(english_cards, 3)
 
         target_word_button = types.KeyboardButton(target_word)
         other_words_buttons = [types.KeyboardButton(word) for word in other_words]
@@ -214,7 +113,7 @@ def get_eng_word(message):
     add_english_word = message.text
     user_id = message.from_user.id
     if add_russian_word not in personal_russian_words and add_english_word not in personal_english_words:
-        print(add_personal_word(engine, add_russian_word, add_english_word, user_id))
+        print(add_personal_word(add_russian_word, add_english_word, user_id))
         bot.send_message(message.chat.id, f'Успешно добавлено слово "{add_russian_word}" с переводом "{add_english_word}" ✅')
     else:
         bot.send_message(message.chat.id,
@@ -241,15 +140,15 @@ def del_word(message):
 def get_del_word(message):
     delate_word = message.text
     uid = message.from_user.id
-    try:
-        if delete_word(delate_word, uid) is str:
-            bot.send_message(message.chat.id, f'Слово "{delate_word}" было успешно удалено.')
-        elif delete_word(delate_word, uid) == 0:
-            bot.send_message(message.chat.id, f'Слово "{delate_word}" не может быть удалено, оно находится в общем доступе.')
-        elif delete_word(delate_word, uid) == 1:
-            bot.send_message(message.chat.id, f'Слово "{delate_word}" не может быть удалено. Возможно, вы его еще не добавили.')
-    except:
-        print('Ошибка')
+    # try:
+    if delete_word(delate_word, uid) is str:
+        bot.send_message(message.chat.id, f'Слово "{delate_word}" было успешно удалено.')
+    elif delete_word(delate_word, uid) == 0:
+        bot.send_message(message.chat.id, f'Слово "{delate_word}" не может быть удалено, оно находится в общем доступе.')
+    elif delete_word(delate_word, uid) == 1:
+        bot.send_message(message.chat.id, f'Слово "{delate_word}" не может быть удалено. Возможно, вы его еще не добавили.')
+    # except:
+    #     print('Ошибка')
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def message_reply(message):
--- PyCode/main.py
+++ PyCode/main.py
@@ -1,33 +1,105 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from sql_func import create_tables, Basic_words, User
-# import psycopg2
-
+from sql_func import User, Personal_words, Basic_words, create_tables
 
 DSN = f"postgresql://postgres:2709200227092002@localhost:5432/translation"
 engine = sqlalchemy.create_engine(DSN)
 create_tables(engine)
 
-def add_basic_word(engine, russian_word: str, english_word: str): #добавляем основные 10 слов
-    session = (sessionmaker(bind=engine))()
+session = (sessionmaker(bind=engine))()
+
+personal_russian_words = []   #список с персональными русскими словами
+personal_english_words = []   #список с персональными английскими словами
+english_cards = []  #список со всеми английскими словами, использующийся для создания карточек
+russian_cards = []  #список со всеми русскими словами, использующийся для создания карточек
+
+basic_russian_words = [i.russian_word for i in (sessionmaker(bind=engine))().query(Basic_words).all()] #список с базовыми русскими словами
+basic_english_words = [i.english_word for i in (sessionmaker(bind=engine))().query(Basic_words).all()] #список с базовыми английскими словами
+
+#Функия получения перевода слов
+def get_target_word(translate_word: str) -> str:
+    if translate_word in basic_russian_words:
+        target_w = session.query(Basic_words.english_word).filter(Basic_words.russian_word == translate_word).all()[0][0]
+        return target_w
+    elif translate_word in basic_english_words:
+        target_w = session.query(Basic_words.russian_word).filter(Basic_words.english_word == translate_word).all()[0][0]
+        return target_w
+    elif translate_word in personal_english_words:
+        target_w = session.query(Personal_words.russian_word).filter(Personal_words.english_word == translate_word).all()[0][0]
+        return target_w
+    elif translate_word in personal_russian_words:
+        target_w = session.query(Personal_words.english_word).filter(Personal_words.russian_word == translate_word).all()[0][0]
+        return target_w
+    else:
+        return 'Нет такого слова'
+#Функция добавления нового пользователя
+def add_users(user_id: int, user_name: str):   #функция добавляющая пользователя в бд с параметрами user_id, user_name
+    session.add(User(user_id=user_id, name=user_name))
+    for i in session.query(User).all():
+        print(i)
+    session.commit()
+#Функция добавления персональных слов пользователем
+def add_personal_word(any_russian_word: str, any_english_word: str, uid: int) -> str: #функция для добавления слов в бд
+    for u_id in session.query(User.id).filter(User.user_id == uid).all():
+        if any_russian_word not in session.query(Personal_words.russian_word).all():
+            session.add(Personal_words(russian_word=any_russian_word, english_word=any_english_word, user_id=u_id[0]))
+            session.commit()
+            for i in session.query(Personal_words).filter(Personal_words.russian_word == any_russian_word).all():
+                result = f'{i.russian_word} - {i.english_word}'
+                personal_russian_words.append(any_russian_word)
+                russian_cards.append(any_russian_word)
+                personal_english_words.append(any_english_word)
+                english_cards.append(any_english_word)
+                return f'"{result}" успешно добавлено.'
+        else:
+            return f'Слово "{any_russian_word}" уже было добавлено ранее'
+#Функция для удаления персональных слов пользователем
+def delete_word(del_word: str, uid: int) -> int:
+    for id_ in session.query(User.id).filter(User.user_id == uid).all():
+        id_ = id_[0]
+        if del_word in basic_english_words or del_word in basic_russian_words:
+            print(f'Попытка удалить слово "{del_word}", что находится в базовых словах.')
+            return 0
+        elif del_word in personal_russian_words:
+            session.query(Personal_words).filter(Personal_words.russian_word == del_word).filter(Personal_words.user_id==id_).delete() # удаляем объект
+            session.commit()
+            del_eng_word = get_target_word(del_word)
+            personal_russian_words.remove(del_word)
+            personal_english_words.remove(del_eng_word)
+            print(f'Удалили слово "{del_word}" из бд')
+            return session.query(Personal_words).filter(Personal_words.user_id==id_).all()[0]
+        elif del_word in personal_english_words:
+            session.query(Personal_words).filter(Personal_words.english_word == del_word).filter(Personal_words.user_id==id_).delete() # удаляем объект
+            session.commit()
+            del_russ_word = get_target_word(del_word)
+            personal_english_words.remove(del_word)
+            personal_russian_words.remove(del_russ_word)
+            print(f'Удалили слово "{del_word}" из бд')
+            return session.query(Personal_words).filter(Personal_words.user_id==id_).all()[0]
+        else:
+            print(f'Попытка удалить слово "{del_word}", которое не существует ни в одном из бд.')
+            return 1
+#Функция для добавления базовых слов
+def add_basic_word(russian_word: str, english_word: str): #добавляем основные 10 слов
     session.add(Basic_words(russian_word=russian_word, english_word=english_word))
     session.commit()
-    session.close()
 
-add_basic_word(engine, 'Масло', 'Oil')
-add_basic_word(engine, 'Магазин', 'Shop')
-add_basic_word(engine, 'Собака', 'Dog')
-add_basic_word(engine, 'Весна', 'Spring')
-add_basic_word(engine, 'Дом', 'House')
-add_basic_word(engine, 'Вода', 'Water')
-add_basic_word(engine, 'Имя', 'Name')
-add_basic_word(engine, 'Дорога', 'Road')
-add_basic_word(engine, 'Рука', 'Arm')
-add_basic_word(engine, 'Одежда', 'Clothes')
+session.close()
 
 
 if __name__ == '__main__':
     from PyCode.bot_func import bot
+
+    add_russian_word = ['Масло', 'Магазин', 'Собака', 'Весна', 'Дом', 'Вода', 'Имя', 'Дорога', 'Рука', 'Одежда']
+    add_english_word = ['Oil', 'Shop', 'Dog', 'Spring', 'House', 'Water', 'Name', 'Road', 'Arm', 'Clothes']
+
+    for r in add_russian_word:
+        russian_cards.append(r)
+    for e in add_english_word:
+        english_cards.append(e)
+
+    print('russian_cards:', russian_cards)
+    print('english_cards:', english_cards)
 
     print('Bot is running')
     bot.polling()
